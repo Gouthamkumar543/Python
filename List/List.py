2ccbf9912f47894235da1c6d16c66e0bad246d87
Gouthamkumar543 <gouthamkumar0345@gmail.com>
2025-02-18 11:55:02 +0530

    added list and tuple

---
+++ List/List.py
@@ -0,0 +1,117 @@
+# # List are mutable and can store multiple datatypes
+
+# std = ["goutham", 21 , "male", 123456 , 'gouthamkumar0345@gmail.com']
+# print(std)
+# print(std[0])
+# print(std[1])
+# print(std[2])
+# print(std[4])
+
+
+# std[3] = 35
+
+# print(std)
+
+# # type 
+# print(type(std))
+
+
+# # lenght of the list
+# print(len(std))
+
+# # List slicing
+# marks = [32,43,6,34,56,24,46,67,46,98]
+# print(marks[3:7])
+# print(marks[:5])
+# print(marks[6:])
+# print(marks[:])
+# print(marks[4:len(marks)])
+# print(len(marks))
+
+
+
+
+
+
+
+# # List methods - append ,sort , reverse , insert , remove , pop
+
+# list = [1,2,3,4]
+
+
+# # append atke sonly one argument
+# list.append(5)
+# list.append(95)
+# print(list)
+
+
+# list =[345,5,54,65,6,7,8,34,5,3456,8,6]
+# list.sort()
+# print(list)
+
+
+# list.sort(reverse=True)
+# print(list)
+
+
+
+# list.reverse()
+# print(list)
+
+# list.reverse(sort=True) # didnt work
+
+
+
+
+# list.insert(3,"goutham")
+# print(list)
+
+# list.insert(5,212345)
+
+
+# list.insert(11,"male")
+# print(list)
+
+
+# # if the index is not present it adds the element in the last
+
+# list.insert(20,"hiiiii")
+# print(list)
+
+
+# list.insert(100,"byeee")
+# print(list)
+
+
+# print(len(list))
+
+
+
+# # removes the elements int he list that occurs the first time
+# list.remove(5)
+# print(list)
+
+# list.remove("goutham")
+# print(list)
+
+
+
+
+# # remove the element from the list at last index 
+# list.pop()
+# list.pop()
+# list.pop()
+# list.pop()
+
+# print(list)
+
+
+# list.extend("goutham")
+# list.extend([23,34,56])
+# list.extend(["goutham","ram"])
+# print(list)
+
+
+
+# print(list.count(5))
+# print(list.count("goutham"))
---
+++ Tuple/Tuple.py
@@ -0,0 +1,36 @@
+# # tuple are immutable
+
+# Tuple = (1,24,45,6765,2456,34,345,1,1,1,1,1,2,2) 
+# print(type(Tuple))
+
+# print(len(Tuple))
+
+
+# # Tuple[3]=35 #not allowed in tuple becz it is immutable
+
+# # tuple methods - index , count
+
+# # print(Tuple[3])
+# # print(Tuple[0])
+# # print(Tuple[6])
+# # print(Tuple[10])
+# # print(Tuple[100]) cant access the elements out of the length of the tuple
+
+
+# print(Tuple.count(1))
+
+
+# Tuple1=(2,2,2,2,2,2,2,2,2,2,2,2,2)
+# Tuple2=(2,2,2,2,2,2,2,2,2,2,2,2,2)
+
+
+# t = Tuple + Tuple1
+# print(t)
+
+
+# i = Tuple * 2
+# print(i)
+
+
+# membership = Tuple2 in Tuple1
+# print(membership)
