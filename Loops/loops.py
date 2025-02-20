bc1a9af7bf7feb91d1ca75287e18c83830ee18b4
Gouthamkumar543 <gouthamkumar0345@gmail.com>
2025-02-20 11:26:05 +0530

    loops file

---
+++ Loops/loops.py
@@ -0,0 +1,117 @@
+# Lopps
+
+# i = 0
+
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+# print ("ended")
+
+
+# numbers from 100 to 1
+
+# i = 100 
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# print("hiii")
+
+
+# n = int(input("enter a number: "))
+# i = 1
+
+# while i <= 10:
+#     print(n*i)
+#     i += 1
+
+
+
+# list = []
+
+# i = 1
+# while i <= 10 :
+#     square = i*i
+#     # print(square)
+#     list.append(square)
+#     i += 1
+
+# print(list)
+
+
+# items = (23,4,5,45,6,5,43,3,456,67,8,6,5445,67,8,876,5)
+
+# i = 0
+# x = 6
+
+# while i < len(items):
+#     if(items[i] == x):
+#         print("number found at",i)
+#     else:
+#         print("not found")
+#     i += 1
+
+
+# break and continue
+
+# i = 0
+
+# while i <= 10:
+#     if(i == 5):
+#         break
+#     print(i)
+#     i += 1
+
+
+# i = 0
+
+# while i <= 10:
+#     if(i == 5):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+
+# list = [1,2,3,4,5,6,7,8,9]
+
+# for i in list:
+#     print(i)
+
+
+
+# numbers = (1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,4,3,2,3,45,67,65,4,5,67)
+# x = 4
+# i = 0
+
+# for elements in numbers:
+#     if (elements == x):
+#         print("found",i)
+#     i += 1
+
+
+
+
+
+# range takes the argumensts as (start, end , step count)
+
+
+# by default it takes stoping condition if passed only one arg () 
+# for i in range(20):
+#     print(i)
+
+# takes start and end arg 
+# for i in range(5,20):
+#     print(i)
+
+
+# for i in range(10,200,10):
+    # print(i)
+
+
+
+# for i in range(0,10):
+#     for j in range(0,10):
+#         print ("i :",i,"and j :",j)
+#         print("i+j =",i+j)
