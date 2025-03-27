27381976e7d7b5e7de824dc5463cf7d8dd79a7de
Gouthamkumar543 <gouthamkumar0345@gmail.com>
2025-03-27 19:59:33 +0530

    ps String

---
+++ PsStrings/StringsPs.py
@@ -0,0 +1,58 @@
+# reverse the string -1
+
+# def Reverse_Srting(x):
+#     new_String = ""
+
+#     for i in range(len(x)-1,-1,-1):
+#         new_String += x[i]
+#     print(new_String)
+
+# string = "goutham"
+# Reverse_Srting(string)
+
+
+# Check if the string is pallinedrome or not -2
+
+# def pallinedrome(x):
+#     p_string = x[::-1]
+#     print(p_string)
+#     if x == p_string:
+#         return print(True)
+#     return print(False)
+
+# string="goutham"
+# pallinedrome(string)
+
+
+
+# Check vowles and update the count -3
+
+# def Vowles_count(x):
+#     count = 0
+#     vowles = "aeiouAEIOU"
+#     for i in x:
+#         if i in vowles:
+#             count += 1
+#     return print(count)
+
+# string = "hellohiiii"
+# Vowles_count(string)
+
+
+#we can take vowles value as single word or array of single letters
+
+
+#removing vowles from the string -4
+
+# def Removing_vowles(x):
+#     new_string = ""
+#     vowles = ["a","e","i","o","u","A","E","I","O","U"]
+#     for i in x:
+#         if i not in vowles:
+#             new_string += i
+#     print(new_string)
+
+# srting = "goutham"
+# Removing_vowles(srting)
+    
+        
